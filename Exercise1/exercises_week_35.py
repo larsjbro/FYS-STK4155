4b7a767d9723212db8b06085ee00558c2adb85ba
larsjbro <lars.johan.brodtkorb@gmail.com>
2024-08-30 21:25:55 +0200

    Exercise week 35
    Add exercise week 35, tasks 2 + 3

--- Exercise1/exercises_week_35.py
+++ Exercise1/exercises_week_35.py
@@ -6,9 +6,9 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-def generate_data(n=100):
+def generate_data(n=100,sigma=0.1):
     x = np.random.rand(n,1)
-    y = 2.0+5*x*x+0.1*np.random.randn(n,1)
+    y = 2.0+5*x*x+sigma*np.random.randn(n,1)
     return x,y
 
 
@@ -53,39 +53,7 @@ def MSE(y_data, y_model):
     return np.sum((y_data - y_model) ** 2) / n
 
 
-def sci_kit_learn_polyfit():
-    def save_fig(fig_id):
-        plt.savefig(image_path(fig_id) + ".png", format='png')
-
-
-
-    x = np.random.rand(100)
-    y = 2.0 + 5 * x * x + 0.1 * np.random.randn(100)
-
-    #  The design matrix now as function of a given polynomial
-    X = np.zeros((len(x), 3))
-    X[:, 0] = 1.0
-    X[:, 1] = x
-    X[:, 2] = x ** 2
-    # We split the data in test and training data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    # matrix inversion to find beta
-    beta = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
-    print(beta)
-    # and then make the prediction
-    ytilde = X_train @ beta
-    print("Training R2")
-    print(R2(y_train, ytilde))
-    print("Training MSE")
-    print(MSE(y_train, ytilde))
-    ypredict = X_test @ beta
-    print("Test R2")
-    print(R2(y_test, ypredict))
-    print("Test MSE")
-    print(MSE(y_test, ypredict))
-
-
-def polynomial_sci_kit(n=100):
+def polynomial_sci_kit(n=100, sigma=0.1):
     from sklearn.linear_model import LinearRegression
     from sklearn.pipeline import Pipeline
     from sklearn.preprocessing import PolynomialFeatures, StandardScaler
@@ -97,7 +65,7 @@ def polynomial_sci_kit(n=100):
     ])
 
 
-    x, y = generate_data(n)
+    x, y = generate_data(n, sigma)
     model.fit(x, y)
     x2 = np.linspace(0,1,)[:, np.newaxis]
     y2 = model.predict(x2)
@@ -109,31 +77,18 @@ def polynomial_sci_kit(n=100):
     plt.title(f'MSE={mse:.3f},R2={r2:.3f}')
     plt.show()
 
-    import os
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    from sklearn.model_selection import train_test_split
 
-    def save_fig(fig_id):
-        plt.savefig(image_path(fig_id) + ".png", format='png')
-
-    def R2(y_data, y_model):
-        return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
-
-    def MSE(y_data, y_model):
-        n = np.size(y_model)
-        return np.sum((y_data - y_model) ** 2) / n
-
-def lecture_note_solution_exercise_2():
+def lecture_note_solution_exercise_2_with_plot(degree=2):
     x = np.random.rand(100)
     y = 2.0 + 5 * x * x + 0.1 * np.random.randn(100)
 
     #  The design matrix now as function of a given polynomial
-    X = np.zeros((len(x), 3))
+    X = np.zeros((len(x), degree+1))
     X[:, 0] = 1.0
-    X[:, 1] = x
-    X[:, 2] = x ** 2
+    for i in range(1,degree+1):
+
+        X[:, i] = x ** i
+
     # We split the data in test and training data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     # matrix inversion to find beta
@@ -142,14 +97,15 @@ def lecture_note_solution_exercise_2():
     # and then make the prediction
     ytilde = X_train @ beta
     print("Training R2")
-    print(R2(y_train, ytilde))
+    r2_train = R2(y_train, ytilde)
     print("Training MSE")
-    print(MSE(y_train, ytilde))
+    mse_train=MSE(y_train, ytilde)
     ypredict = X_test @ beta
     print("Test R2")
-    print(R2(y_test, ypredict))
+    r2_test = R2(y_test, ypredict)
     print("Test MSE")
-    print(MSE(y_test, ypredict))
+    mse_test = MSE(y_test, ypredict)
+
 
     index=np.argsort(X_test[:,1])
     x = X_test[index,1]
@@ -160,7 +116,7 @@ def lecture_note_solution_exercise_2():
     plt.plot(x, y_fit, label='Fitted polynomial', color='blue')
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.title('Second Order Polynomial Fit')
+    plt.title(f'MSE_train_test=({mse_train:.3f},{mse_test:.3f}),R2_train_test=({r2_train:.3f},{r2_test:.3f}),Degree={degree}')
     plt.legend()
     plt.show()
 
@@ -169,6 +125,10 @@ if __name__ == '__main__':
     #main(n=5)
     #linear_regression(n=5)
     #second_order_polynomial_regression(n=100)
-    #sci_kit_learn_polyfit()
-    #polynomial_sci_kit(n=100)
-    lecture_note_solution_exercise_2()
+    #polynomial_sci_kit(n=100, sigma=1)
+    '''
+    Trying with sigma =1 for big noise. I see that the polynomial has a good general fit, 
+    but the data are scattered quite far from the fitted line as shown with R2 = 0.690
+    With Sigma 0.1 the fit and scatter are both close to the fitted line.
+    '''
+    lecture_note_solution_exercise_2_with_plot(degree=5)
